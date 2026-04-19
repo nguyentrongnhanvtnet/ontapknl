d433121a43155118b6a9c50abfea4c04c0ab031d
nguyentrongnhanvtnet <nguyentrongnhan.vtnet@gmail.com>
2026-04-19 14:21:23 +0700

    Update app.js

--- app.js
+++ app.js
@@ -244,3 +244,9 @@ function normalize(c) {
   if (typeof c === "number") return [c];
   return [];
 }
+function shuffleArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+}
